bae49959ee4a1614a2c6c1fbdd6eebb00aefab31
solesie <tkddbs9801@gmail.com>
2024-04-06 18:01:55 +0900

    feat: split ipc_key enum for shm, msgq, sem and add key_t for memory table
    - Split the ipc_key enum into seperate parts for shared memory (shm), message queues (msgq), and semaphores to improve modularity and readability.
    - Added new key_t values in the respective parts to support memory table functionality.

--- proj1/ipc/ipc_keys.h
+++ proj1/ipc/ipc_keys.h
@@ -1,21 +1,42 @@
+/*
+ * define System-V5 key_t value
+ */
+
 #ifndef IPC_KEYS_H
 #define IPC_KEYS_H
 
-/* define System-V5 key_t value */
-enum IpcKey {
+enum sem_keys {
+	_KEY_T_SEM_START = 0x1000,
+
 	/* stoage table semaphore */
-	KEY_T_SEM_STORAGE_TABLE = 0x1234,
+	KEY_T_SEM_ST,
 	
 	/* shared memory semaphores */
 	KEY_T_SEM_SHM_INPUT,
 	KEY_T_SEM_SHM_OUTPUT,
+	KEY_T_SEM_SHM_MT,
+
+	_KEY_T_SEM_END
+};
+
+enum shm_keys {
+	_KEY_T_SHM_START = 0x2000,
 
 	/* shared memory */
 	KEY_T_SHM_INPUT,
 	KEY_T_SHM_OUTPUT,
+	KEY_T_SHM_MT,
 
-	/* message queue */
-	KEY_T_MSG_QUEUE
+	_KEY_T_SHM_END
 };
 
-#endif
+enum msgq_keys {
+	_KEY_T_MSGQ_START = 0x3000,
+
+	/* message queue */
+	KEY_T_MSGQ,
+
+	_KEY_T_MSGQ_END
+};
+
+#endif // IPC_KEYS_H
